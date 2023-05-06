9a4692d0e976637db04840c8f84992aeca2b7f45
vgenique <40019634+vgenique@users.noreply.github.com>
2023-05-06 09:40:39 +0200

    Update app.py
    add header
    add 3 lines

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
 import streamlit as st
 
 st.title('Snowflake App: My Parents new healthy diner')
+st.header('Breakfast menu')
+st.text('Omega 3 & Blueberry Oatmeal')
+st.text('Kale, Spinach & Rocket smoothie')
+st.text('Hard-Boiled free-range egg')
