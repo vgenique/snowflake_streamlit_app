c0082582018ed83a7f32f847d45b85469159b7ab
vgenique <40019634+vgenique@users.noreply.github.com>
2023-05-09 10:42:58 +0200

    Update app.py
    add multiselect on dataframe

--- app.py
+++ app.py
@@ -10,4 +10,9 @@ st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 import pandas as pd
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+# Let's put a pick list here so they can pick the fruit they want to include 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
+# Display the table on the page.
 st.dataframe(my_fruit_list)
