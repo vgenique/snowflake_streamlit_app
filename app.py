646420e3f1fe94e894ed7e01d2fa5d13baad9c7a
vgenique <40019634+vgenique@users.noreply.github.com>
2023-05-09 10:41:48 +0200

    Update app.py
    load fruit list
    create dataframe from list

--- app.py
+++ app.py
@@ -7,3 +7,7 @@ st.text('🥗 Kale, Spinach & Rocket Smoothie')
 st.text('🐔 Hard-Boiled Free-Range Egg')
 st.text('🥑🍞 Avocado Toast')
 st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+import pandas as pd
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+st.dataframe(my_fruit_list)
